2d795cf52108501fd958050ac6aeba4f660bb250
pasinduweerarathne <pasiweerarathne98@gmail.com>
2024-02-06 14:05:32 +0530

    setup redux in the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/routes";
+import { Provider } from 'react-redux'
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </>
+  </Provider>
 );
